693239b7c9fa676eb073694fbdfc8f63734c6c10
Carchedi <carchedi.ufjf@gmail.com>
2023-08-14 16:34:53 -0300

    button showing modal, but not the button I need

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -1,17 +1,18 @@
 import './banner.css'
 function Banner(props){
+ 
     return( 
-        <header class="masthead text-center d-flex">
-            <div class="container my-auto">
-                <div class="row">
-                    <div class="col-lg-10 mx-auto">
-                        <h1 class="text-uppercase">
+        <header className="masthead text-center d-flex">
+            <div className="container my-auto">
+                <div className="row">
+                    <div className="col-lg-10 mx-auto">
+                        <h1 className="text-uppercase">
                             <strong>{props.mainText}</strong>
                         </h1>
                          <h6>{props.caption}</h6>
 
                          <hr></hr>
-                        <button type='button' className='btn btn-secondary btn-xl js-scrll-trigger'>
+                        <button variant="primary" type='button' className='btn btn-secondary btn-xl js-scrll-trigger'>
                             {props.chooseLanguage}
                         </button> 
                     </div>
