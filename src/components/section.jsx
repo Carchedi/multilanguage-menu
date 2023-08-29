141d47dbd9b148fa793c51b5fbc78d5c9720b096
Carchedi <carchedi.ufjf@gmail.com>
2023-08-29 13:35:48 -0300

    Contents components

--- src/components/section.jsx
+++ src/components/section.jsx
@@ -4,11 +4,14 @@ function Section(props){
     return(
         <div id={props.name} className="col-md-12">
             <h1>{props.name}</h1>
-            Lorem ipsum dolor sit amet. Et commodi temporibus non impedit assumenda et tenetur temporibus ut molestiae nemo in enim soluta. In esse incidunt sit consequatur doloremque aut iste distinctio.
-
-Aut quis blanditiis ad quam similique sed quas repellat et atque temporibus in iusto harum a voluptatem consectetur. In explicabo sunt non quia amet sit nihil enim est deserunt molestiae.
-
-Id optio distinctio quo voluptatem ipsum eos facilis reiciendis id aliquam iure qui velit sint ut commodi iusto! Sit dignissimos facere aut omnis perferendis et dignissimos natus et asperiores fugit. Ea vero consectetur a libero inventore nam ipsa nisi. Ut facere consequatur hic suscipit fugiat qui autem exercitationem non earum iusto non dolorem maiores 33 maiores consequuntur aut omnis nihil.
+            <hr/>
+            Lorem ipsum dolor sit amet. Et commodi temporibus non impedit assumenda et tenetur temporibus ut molestiae nemo in enim 
+            soluta. In esse incidunt sit consequatur doloremque aut iste distinctio. Aut quis blanditiis ad quam similique sed quas
+            repellat et atque temporibus in iusto harum a voluptatem consectetur. In explicabo sunt non quia amet sit nihil enim est
+            deserunt molestiae. Id optio distinctio quo voluptatem ipsum eos facilis reiciendis id aliquam iure qui velit sint ut
+            commodi iusto! Sit dignissimos facere aut omnis perferendis et dignissimos natus et asperiores fugit. Ea vero consectetur
+             a libero inventore nam ipsa nisi. Ut facere consequatur hic suscipit fugiat qui autem exercitationem non earum iusto non
+             dolorem maiores 33 maiores consequuntur aut omnis nihil.
         </div>
     )
 }
