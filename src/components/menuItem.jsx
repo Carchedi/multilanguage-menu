0eed43c6af62d5525d8cebfa2b42cfaa1eebd028
Carchedi <carchedi.ufjf@gmail.com>
2023-09-01 16:54:48 -0300

    images in a correct size

--- src/components/menuItem.jsx
+++ src/components/menuItem.jsx
@@ -4,7 +4,7 @@ function MenuItem(props){
         <div class="card border-secondary mb-2" >
             <div class="card-header">{props.itemName}</div>
             <div class="card-body text-secondary">
-                <img src={props.imgSrc} alt={props.imgAlt}/>
+                <img src={props.imgSrc} alt={props.imgAlt} style={{width:"100%", height:"100%"}}/>
                 <p class="card-text">{props.itemDescription}</p>
             </div>
         </div>
